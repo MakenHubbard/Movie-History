9c63e37fb76537ed56a00c6cdc87643e1035ba0c
Maken Hubbard <makhub26@outlook.com>
2018-05-21 21:30:55 -0500

    adding the navbar links ticket

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,3 +1,21 @@
 const myLinks = () => {
+  $(document).click((e) => {
+    if (e.target.id === 'login') {
+      $('#myMovies').addClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').removeClass('hide');
+    } else if (e.target.id === 'favMovies') {
+      $('#myMovies').removeClass('hide');
+      $('#search').addClass('hide');
+      $('#authScreen').addClass('hide');
+    } else if (e.target.id === 'searchMovies') {
+      $('#myMovies').addClass('hide');
+      $('#search').removeClass('hide');
+      $('#authScreen').addClass('hide');
+    }
+  });
+};
 
-}
+module.exports = {
+  myLinks,
+};
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,3 @@
+const events = require('./events');
 
+events.myLinks();
